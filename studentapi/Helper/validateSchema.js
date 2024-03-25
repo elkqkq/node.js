a37bec5385ac926e032091a54e4ffe85b5ec8d69
Abdinasir <samiradynassir@gmail.com>
2024-03-25 11:51:53 +0300

    added bcrypt password

--- studentapi/Helper/validateSchema.js
+++ studentapi/Helper/validateSchema.js
@@ -1,26 +1,26 @@
-// const Joi = require("joi");
+const Joi = require("joi");
 
-// const authSchema = Joi.object({
-//     regName: Joi.string().required(),
-//     regEmail: Joi.string().email().lowercase().required(),
-//     regPassword: Joi.string().min(6).required(),
-// });
+const authSchema = Joi.object({
+    regName: Joi.string().required(),
+    regEmail: Joi.string().email().lowercase().required(),
+    regPassword: Joi.string().min(6).required(),
+});
 
-// module.exports = {authSchema};
-const Joi = require('joi');
+module.exports = {authSchema};
+// const Joi = require('joi');
 
-const authSchema = Joi .object({
-    email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(6).required(),
-})
+// const authSchema = Joi .object({
+//     email: Joi.string().email().lowercase().required(),
+//     password: Joi.string().min(6).required(),
+// })
 
-const vendorValidate = Joi.object ({
-    first_name: Joi.string().required(),
-    last_name: Joi.string().required(),
-    phonenumber: Joi.number().required(),
-    email: Joi.string().email().required(),
-    address: Joi.string(),
-})
+// const vendorValidate = Joi.object ({
+//     first_name: Joi.string().required(),
+//     last_name: Joi.string().required(),
+//     phonenumber: Joi.number().required(),
+//     email: Joi.string().email().required(),
+//     address: Joi.string(),
+// })
 
 
-module.exports = {authSchema, vendorValidate}
+// module.exports = {authSchema, vendorValidate}
--- studentapi/model/regModel.js
+++ studentapi/model/regModel.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
     const Reg = sequelize.define('registration', {
         reg_id: {
@@ -17,28 +19,29 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
-    });
-
-    // Function to hash password before saving
-    Reg.beforeCreate( async (user) => {
-        try {
-            const salt = await bcrypt.genSalt(12);
-            const hashedPwd = await bcrypt.hash(user.regPassword, salt)
-            user.regPassword = hashedPwd;
-        } catch (error) {
-            throw new Error("Error encrypting password")
+    }, {
+        hooks: {
+            // Hook before creating an instance of the model
+            beforeCreate: async (reg) => {
+                try {
+                    const salt = await bcrypt.genSalt(12);
+                    const hashedPwd = await bcrypt.hash(reg.regPassword, salt);
+                    reg.regPassword = hashedPwd;
+                } catch (error) {
+                    throw new Error("Error encrypting password");
+                }
+            }
         }
-    })
-
+    });
+    
     // Function to compare the entered password with the saved hashed password
     Reg.prototype.isValidPassword = async function(password) {
         try {
-            return await bcrypt.compare(password, this.password);
+            return await bcrypt.compare(password, this.regPassword);
         } catch (error) {
             throw error;
         }
     };
+
     return Reg;
-}
-
-
+};
